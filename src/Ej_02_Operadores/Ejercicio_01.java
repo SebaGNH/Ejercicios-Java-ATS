f4de2d39328a82ac86ba1a12ffade858f2eb4c94
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-04-04 17:58:06 -0300

    Se agregó ejercicio 1

---
+++ src/Ej_02_Operadores/Ejercicio_01.java
@@ -0,0 +1,18 @@
+public class Ejercicio_01 {
+/*
+Hacer un programa que calcule e imprima la suma de tres calificaciones
+*/
+
+public static void main(String[] args) throws Exception{
+    
+    int num1 = 3;
+    int num2 = 4;
+    int num3 = 9;
+
+    int suma = num1 + num2 + num3;
+
+    System.out.println(suma);
+
+}
+
+}
