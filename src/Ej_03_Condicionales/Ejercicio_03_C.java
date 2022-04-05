51662f40d7f8663ea4b24b4950c110ae2bf47161
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-04-05 18:09:49 -0300

    Ejercicio 3 condicionales completo

--- src/Ej_03_Condicionales/Ejercicio_03_C.java
+++ src/Ej_03_Condicionales/Ejercicio_03_C.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Ejercicio_03_C {
 /*
 Programa que lea un caracter por teclado y compruebe si es una letra mayúscula
 */
+
+public static void main(String[] args) throws Exception{
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Ingrese una letra para saber si es mayuscula o minuscula: ");
+    char caracter = sc.next().charAt(0);
+
+    if (Character.isUpperCase(caracter)) {
+        System.out.println("\nEs mayuscula");
+    } else {
+        System.out.println("\nNo es mayuscula");
+    }
+    
+    sc.close();
+}
 }
